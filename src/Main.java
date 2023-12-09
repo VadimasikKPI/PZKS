08e9282e5611c34edcaf306f75e816a6cba7a953
VadimasikKPI <fhghvhg314@gmail.com>
2023-12-09 15:13:01 +0200

    5 lab

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //String expression = "a/(b-c)";
-        String expression = "a-(b*a-c-a*b*c)+b*c+c+c*d*e+e*f+k*(d+f+2*(a-c*e+w)-q*t)";//якщо не має * чи / то душку прибрати (це перед обробкою зробити)
-        //String expression = "a+b*c-d*a";
-        //String expression = "a+(b+c)-d";
-        //String expression1 = "(a/(b-c)-b)";
-        //String expression1 = "3*d*(a+a*b*c)";
-        //String expression1 = "a*e*(b*c-c*d-b+b/d)";
-        //String expression1 = "a*e*(b*c+c*d*(w-f)-b+b/d)";
-        String expression1 = "a-b*(k-t)-(f-g)*(f*5.9-q)-(f+g)/(d+q-w)";
-//        Analyzer analyzer = new Analyzer();
-//        List<String> errors = analyzer.analyze(expression);
-        //String expression = "-f(c, e++, 4m)/h[k+i] - 2.0005^(N-1)-(1-L) + s(t_1+e*m) - 1,7/kgh+b-g(e, 1)-(1- 0)";
+        laba5();
+    }
+
+    public static void laba5(){
         //String part = "a+(b+c+d+(e+f)+g)+h+f(a)";
         //String part = "((a + f(x)) + (c + d)) + ((e + f) + (g + h))-f(x)";
         String part = "a*b*c/d + e*f*g/h + t*(a-q) - 5.0*i - 4*j + k + L + m*n*k*(p-1) + sin(pi*R)*log(q)/sin(3*pi/4 + x*pi/2)";
-
         Analyzer analyzer = new Analyzer();
         ExpressionConverter expressionConverter = new ExpressionConverter();
         Worker worker = new Worker();
@@ -46,6 +37,5 @@ public class Main {
         Matrix matrix = new Matrix();
         //matrix.manageOperations(rebuild);
         matrix.processTree(rebuild);
-
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -245,7 +245,7 @@ public class Matrix {
                         else{
                             for(int k = 0;k<processors.size();k++){
                                 if(processors.get(k).size()!=processors.get(processorNumber).size()){
-                                    for(int l = 0;l<processors.get(processorNumber).size();l++){
+                                    while(processors.get(k).size()!=processors.get(processorNumber).size()){
                                         processors.get(k).add("Empty");
                                     }
                                 }
@@ -392,21 +392,22 @@ public class Matrix {
                                     }
                                 }
                             }
+
                             for(int l = 0;l<calculateNumberOfInputs(operationsMap.get(sorted.get(i)).getOperation());l++){
                                 processors.get(secondProcessorNumber).add(sorted.get(i).toString());
                             }
                             time = calculateTime(processors);
-                            if(time<tempTime && !priority.contains(j)){
+                            if(time<tempTime && priority.contains(j)){
                                 tempTime = time;
-                                processorNumber = j;
+                                processorNumber = secondProcessorNumber;
                                 taskNumber = sorted.get(i);
                                 previousTaskNumber = sorted.get(i);
                                 resultTime = time;
                             }
-                            else if(time==tempTime && !priority.contains(j)){
+                            else if(time==tempTime && priority.contains(j)){
                                 if(processors.get(j).size()<processors.get(processorNumber).size()){
                                     tempTime = time;
-                                    processorNumber = j;
+                                    processorNumber = secondProcessorNumber;
                                     taskNumber = sorted.get(i);
                                     previousTaskNumber = sorted.get(i);
                                     resultTime = time;
@@ -473,17 +474,17 @@ public class Matrix {
                                 processors.get(firstProcessorNumber).add(sorted.get(i).toString());
                             }
                             time = calculateTime(processors);
-                            if(time<tempTime && !priority.contains(j)){
+                            if(time<tempTime && priority.contains(j)){
                                 tempTime = time;
-                                processorNumber = j;
+                                processorNumber = firstProcessorNumber;
                                 taskNumber = sorted.get(i);
                                 previousTaskNumber = sorted.get(i);
                                 resultTime = time;
                             }
-                            else if(time==tempTime && !priority.contains(j)){
+                            else if(time==tempTime && priority.contains(j)){
                                 if(processors.get(j).size()<processors.get(processorNumber).size()){
                                     tempTime = time;
-                                    processorNumber = j;
+                                    processorNumber = firstProcessorNumber;
                                     taskNumber = sorted.get(i);
                                     previousTaskNumber = sorted.get(i);
                                     resultTime = time;
